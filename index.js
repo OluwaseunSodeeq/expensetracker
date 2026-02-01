785eb5b1ed79e2ddb2b7fd915afaa56356a92c4b
OluwaseunSodeeq <ademolaoluwaseun90@gmail.com>
2026-02-01 19:55:52 +0100

    Merge remote main with local changes

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // =========================DECLERATION OF VARIABLES===========================
 
-const $firstButton = document.getElementsByClassName("firstButton")[0];
-const $inputList = document.querySelector(".inputList");
-const $formerWithin = document.getElementsByTagName("button")[1];
-const $latterWithin = document.getElementsByClassName("latterWithin")[0];
+const firstButton = document.getElementsByClassName("firstButton")[0];
+const inputList = document.querySelector(".inputList");
+const formerWithin = document.getElementsByTagName("button")[1];
+const latterWithin = document.getElementsByClassName("latterWithin")[0];
 const allInputs = document.querySelectorAll("input");
 const aggregate = document.querySelector(".total");
 const btnRemover = document.querySelector(".removedButton");
 
 // =========================To make the Input-Lists come to live =========================
-$firstButton.onclick = function () {
-  $inputList.style.display = "block";
+firstButton.onclick = function () {
+  inputList.style.display = "block";
 };
 
 // ========================= To hide the input-lists and to clear whatever the users enter into the input =========================
-$formerWithin.onclick = function () {
+formerWithin.onclick = function () {
   allInputs.forEach((input) => {
     input.value = "";
   });
 
-  $inputList.style.display = "none";
+  inputList.style.display = "none";
 };
 
-$latterWithin.onclick = function () {
-  let $firstInput = "";
-  let $secondInput = 0;
-  let $thirdInput = "";
+latterWithin.onclick = function () {
+  let firstInput = "";
+  let secondInput = 0;
+  let thirdInput = "";
 
   if (
     allInputs[0].value.trim() === "" ||
@@ -40,20 +40,20 @@ $latterWithin.onclick = function () {
 
   allInputs.forEach((input) => {
     if (input.type === "text") {
-      $firstInput = input.value;
+      firstInput = input.value;
     } else if (input.type === "number") {
-      $secondInput = input.value;
+      secondInput = input.value;
     } else if (input.type === "date") {
-      $thirdInput = input.value;
+      thirdInput = input.value;
     }
   });
 
   document.querySelector(".total").innerHTML += newExpense(
-    $firstInput,
-    $secondInput,
-    $thirdInput,
+    firstInput,
+    secondInput,
+    thirdInput,
   );
-  $inputList.style.display = "none";
+  inputList.style.display = "none";
 
   allInputs.forEach((input) => {
     input.value = "";
@@ -63,7 +63,7 @@ $latterWithin.onclick = function () {
 };
 
 //============This is where i defined the funtion for adding new expense================
-function newExpense($firstInput, $secondInput, $thirdInput) {
+function newExpense(firstInput, secondInput, thirdInput) {
   // An array is created for months
   const months = [
     "Jan",
@@ -81,7 +81,7 @@ function newExpense($firstInput, $secondInput, $thirdInput) {
   ];
 
   // This is where i deeclared variable for Date and its Items and an hyphen is used to split the Items
-  const date = $thirdInput.split("-");
+  const date = thirdInput.split("-");
   const year = date[0];
   const month = date[1];
   const day = date[2];
@@ -94,8 +94,8 @@ function newExpense($firstInput, $secondInput, $thirdInput) {
                         <p>${months[month - 1]}</p>
                         <p>${year}</p>
                     </div>
-                    <p id="item">${$firstInput}</p>
-                    <p id="amount">$${$secondInput}</p>
+                    <p id="item">${firstInput}</p>
+                    <p id="amount">${secondInput}</p>
                     <button class="removedButton">Remove</button>
         </div>
 
